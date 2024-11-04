1f48498498cd1d0b51e235c271397843363940f5
baptiste <baptiste.c.roux@gmail.com>
2024-11-04 18:34:44 +0100

    fix maj avatar

---
+++ src/environments/environment.mobile.ts
@@ -0,0 +1,5 @@
+// Nouvel environnement pour mobile
+export const environment = {
+  production: false,
+  apiUrl: 'https://spin-api-tjx5.onrender.com/api', // Pour l'iPhone
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+// Environnement par défaut (développement web)
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api', // ou le port que tu utilises pour ton API
+  apiUrl: 'http://localhost:3000/api'  // Pour ng serve
 };
 
 /*
